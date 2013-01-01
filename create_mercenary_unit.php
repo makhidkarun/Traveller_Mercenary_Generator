22d08e0b4908b83bd0eb216d2cad9ab597ec312f
Leam Hall <leamhall@gmail.com>
2013-01-01 00:38:22 -0500

    Version with First names and adjustable unit sizes.

--- create_mercenary_unit.php
+++ create_mercenary_unit.php
@@ -7,11 +7,13 @@
 
 
 require_once 'create_mercenary_unit_include.php';
-require_once 'names.php';
+require_once 'last_names.php';
 
+/* Defaults */ 
 $troop_count = array ( 'troops_per_squad' => 8, 'squads_per_platoon' => 3, 'platoons_per_company' => 4);
 $stat_parameters = array ('min_stat' => 2,'min_physical_stat' => 6,'min_mental_stat' => 5,'max_stat' => 10);
-
+ 
+/* Change from defaults if requested. */
 if (array_key_exists('_submit_check', $_POST)) {
     foreach($troop_count as $k => $v) {
         if (isset($_POST[$k]) && is_numeric($_POST[$k])) {
@@ -23,9 +25,17 @@ if (array_key_exists('_submit_check', $_POST)) {
             $stat_parameters[$k] = $_POST[$k];
         }
     }
+	foreach ($stat_parameters as $k => $v ) {
+		if ( $stat_parameters[$k] > $stat_parameters['max_stat']) {
+			$stat_parameters[$k] = $stat_parameters['max_stat'];
+		} else if ($stat_parameters[$k] < $stat_parameters['min_stat']) {
+			$stat_parameters[$k] = $stat_parameters['min_stat'];
+		}
+	}
+
 }
 
-
+/* Write the page */
 echo <<< _HTML_
     <html>
     <head>
@@ -35,28 +45,28 @@ echo <<< _HTML_
     <h2>Traveller Mercenary Generator</h2>
     <p>Adjust defaults if you want and then hit "Sign them up!"
 
-    <form method="POST" action="{$_SERVER[PHP_SELF]}">
+    <form method="POST" action="{$_SERVER['PHP_SELF']}">
     <table cellpadding="20" width="90%">
     <tr><td align="top">
     <h3>Individual Stat Parameters</h3>
     <p>Minimum Stat (2-15)
-        <input type="number" min="2" max="15" name="min_stat" value="{$stat_parameters[min_stat]}" size="5">
+        <input type="number" min="2" max="15" name="min_stat" value="{$stat_parameters['min_stat']}" size="5">
     <p>Minimum Physical Stat (2-15)
-        <input type="number" min="2" max="15" name="min_physical_stat" value="{$stat_parameters[min_physical_stat]}" size="5">
+        <input type="number" min="2" max="15" name="min_physical_stat" value="{$stat_parameters['min_physical_stat']}" size="5">
     <p>Minimum Mental Stat (2-15)
-        <input type="number" min="2" max="15"name="min_mental_stat" value="{$stat_parameters[min_mental_stat]}" size="5">
+        <input type="number" min="2" max="15"name="min_mental_stat" value="{$stat_parameters['min_mental_stat']}" size="5">
     <p>Maximum Stat (2-15)
-        <input type="number" min="2" max="15" name="max_stat" value="{$stat_parameters[max_stat]}" size="5">
+        <input type="number" min="2" max="15" name="max_stat" value="{$stat_parameters['max_stat']}" size="5">
     </td>
     <td align="top">
     <h3>TOE Parameters</h3>
 
     <p>Troops per Squad (1-15)
-        <input type="number" min="1" max="15" name="troops_per_squad" value="{$troop_count[troops_per_squad]}" size ="5">
+        <input type="number" min="1" max="15" name="troops_per_squad" value="{$troop_count['troops_per_squad']}" size ="5">
     <p>Squads Per Platoon (1-8 )
-        <input type="number" min="1" max="8" name="squads_per_platoon" value="{$troop_count[squads_per_platoon]}" size ="5">
+        <input type="number" min="1" max="8" name="squads_per_platoon" value="{$troop_count['squads_per_platoon']}" size ="5">
     <p>Platoons per Company (1-8)
-        <input type="number" min="1" max="8" name="platoons_per_company" value="{$troop_count[platoons_per_company]}" size ="5">
+        <input type="number" min="1" max="8" name="platoons_per_company" value="{$troop_count['platoons_per_company']}" size ="5">
 
     <br>
     <input type="hidden" name="_submit_check" value="1">
@@ -71,5 +81,8 @@ echo <<< _HTML_
 
 _HTML_;
 
-generate_company($troop_count, $stat_parameters, $names, $unit_id, $hq_staff_roles, $rank_structure,      $rank_structure, $base_skills, $all_skills);
-?></pre></body></html>
+generate_company($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $hq_staff_roles, $rank_structure, $rank_structure, $base_skills, $all_skills);
+
+
+close_web_page();
+?>
--- create_mercenary_unit_include.php
+++ create_mercenary_unit_include.php
@@ -5,6 +5,9 @@
 	version 0.1
 */
 
+require_once 'male_first_names.php';
+require_once 'female_first_names.php';
+
 if (! isset($troop_counttroops_per_squad)) {
 	$troop_count = array (
 		'troops_per_squad' => 8,
@@ -106,9 +109,14 @@ $all_skills = array (
  Functions 
 */
 
-function get_name($names) {
-	$i = rand(0, count($names));
-	return $names[$i];
+function get_last_name($last_names) {
+	$i = rand(0, count($last_names));
+	return $last_names[$i];
+}
+
+function get_first_name($first_names) {
+	$i = rand(0, count($first_names));
+	return $first_names[$i];	
 }
 
 function generate_stats($stat_parameters) {
@@ -132,16 +140,19 @@ function add_skill($skill_array, $skill) {
 	return $skill_array;
 }
 
-function generate_person( $rank_min, $rank_max, $role, $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) {
+function generate_person( $rank_min, $rank_max, $role, $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) {
 	$upp = generate_stats($stat_parameters);
 	$rank = $rank_structure[rand($rank_min, $rank_max)];
 	$age = 18 + rand(0,5) + $rank_min; 
 	if ( rand(1,6) <=5 ) {
 		$gender = 'Male';
+		$first_name = get_first_name($male_first_names);
 	} else {
 		$gender = 'Female';
+		$first_name = get_first_name($female_first_names);
 	}
-	$name = get_name($names);
+	
+	$last_name = get_last_name($last_names);
 	$skill_array = array();
 	$skill_count = rand(1,6) + $rank_max/2;
 	$max_skill_level = 12 + ($rank_max * 2);
@@ -162,7 +173,7 @@ function generate_person( $rank_min, $rank_max, $role, $stat_parameters, $names,
 		$skill_array = add_skill($skill_array, $all_skills[rand(1, $max_skill_level)]);
 	}	
 	echo "\t";
-	echo "$rank $name, $upp $gender $age  ";
+	echo "$rank $first_name $last_name, $upp $gender $age  ";
 	echo "\n\t";
 	foreach ($skill_array as $skill => $level) {
 		echo "$skill - $level ";
@@ -170,67 +181,67 @@ function generate_person( $rank_min, $rank_max, $role, $stat_parameters, $names,
 	echo "\n";
 }
 	
-function generate_squad($troop_count, $stat_parameters, $names, $unit_id, $unit_number, $rank_structure, $base_skills, $all_skills) {
+function generate_squad($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $unit_number, $rank_structure, $base_skills, $all_skills) {
 	
 	$UNIT = $unit_id[$unit_number];
 	echo "\n$UNIT Squad:\n";
 	
 	if ($UNIT == '1st') {
-		generate_person( 6, 7, 'nco', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-		generate_person( 2, 3, 'medic', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person( 6, 7, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person( 2, 3, 'medic', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 	} else {
-		generate_person( 5, 6, 'nco', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person( 5, 6, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 	}
 	
 	for ( $i = 1; $i <= $troop_count['troops_per_squad']; $i++) {
 		if ( $i == 1) {
 			echo "\n";
-			generate_person( 3, 4, 'nco', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-		} elseif ($i == 5) {
+			generate_person( 3, 4, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		} elseif (( $i % 4) == 0) {
 			echo "\n";
-			generate_person( 2, 3, 'troop', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+			generate_person( 2, 3, 'troop', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 		} else {
-			generate_person( 1, 1, 'troop', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+			generate_person( 1, 1, 'troop', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 		}
 	}
 }
 
-function generate_platoon($troop_count, $stat_parameters, $names, $unit_id, $unit_number, $rank_structure, $rank_structure, $base_skills, $all_skills) {
+function generate_platoon($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $unit_number, $rank_structure, $rank_structure, $base_skills, $all_skills) {
 
 	$UNIT = $unit_id[$unit_number];
 	echo "\n$UNIT Platoon: \n";
 	
 	if ( $UNIT == '1st') {	
-		generate_person( 11, 11, 'officer', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person( 11, 11, 'officer', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 	} else {
-		generate_person( 10, 11, 'officer', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person( 10, 11, 'officer', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 	}
 		
 	for ( $i = 1; $i <= $troop_count['squads_per_platoon']; $i++) {	
-		generate_squad($troop_count, $stat_parameters, $names, $unit_id, $i , $rank_structure, $base_skills, $all_skills);
+		generate_squad($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $i , $rank_structure, $base_skills, $all_skills);
 	}
 }
 
-function generate_company($troop_count, $stat_parameters, $names, $unit_id, $hq_staff_roles, $rank_structure, $rank_structure, $base_skills, $all_skills) {
+function generate_company($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $hq_staff_roles, $rank_structure, $rank_structure, $base_skills, $all_skills) {
 	echo "Commander: \n";
-	generate_person( 12, 13, 'officer', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 12, 13, 'officer', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 	
-	generate_hq_staff($hq_staff_roles, $stat_parameters, $names, $rank_structure, $base_skills, $all_skills);
+	generate_hq_staff($hq_staff_roles, $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills);
 
 	for ( $i = 1; $i <= $troop_count['platoons_per_company']; $i++ ) {
-		generate_platoon($troop_count, $stat_parameters, $names, $unit_id, $i, $rank_structure, $rank_structure, $base_skills, $all_skills);
+		generate_platoon($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $i, $rank_structure, $rank_structure, $base_skills, $all_skills);
 	}
 }
 
-function generate_hq_staff($hq_staff_roles, $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) {
+function generate_hq_staff($hq_staff_roles, $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) {
 	echo "HQ Platoon:\n";
-	generate_person( 7, 9, 'nco', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 4, 6, 'medic', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 2, 3, 'comm', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 2, 3, 'sensors', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 2, 3, 'clerk', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 1, 2, 'driver', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
-	generate_person( 1, 2, 'driver', $stat_parameters, $names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 7, 9, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 4, 6, 'medic', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 2, 3, 'comm', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 2, 3, 'sensors', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 2, 3, 'clerk', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 1, 2, 'driver', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	generate_person( 1, 2, 'driver', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
 }
 /*
 function open_web_page($troop_count, $stat_parameters, $_POST) {
