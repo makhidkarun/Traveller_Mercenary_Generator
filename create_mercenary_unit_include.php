fcb500cd65097baae4f1ade5fea664ab8ec525bb
Leam Hall <leamhall@gmail.com>
2013-08-17 09:34:54 -0400

    Added functions for JSON builds. Assumes output 'mercenary.js'.

--- create_mercenary_unit_include.php
+++ create_mercenary_unit_include.php
@@ -199,9 +199,77 @@ function generate_person( $rank_min, $rank_max, $role, $stat_parameters, $last_n
 	}
 	echo "\n";
 }
+
+function generate_person_json( $rank_min, $rank_max, $role, $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) {    
+    global $mercenary_js;
+	$upp = generate_stats($stat_parameters);
+	$rank = $rank_structure[rand($rank_min, $rank_max)];
+	$age = 18 + rand(0,5) + $rank_min; 
+	if ( rand(1,6) <=5 ) {
+		$gender = 'Male';
+		$first_name = get_first_name($male_first_names);
+	} else {
+		$gender = 'Female';
+		$first_name = get_first_name($female_first_names);
+	}
 	
+	$last_name = get_last_name($last_names);
+	$skill_array = array();
+	$skill_count = rand(1,6) + $rank_max/2;
+	$max_skill_level = 12 + ($rank_max * 2);
+	if ( $rank_max > 8 ) {
+		$max_skill_level = count($all_skills);
+	} else {
+		$max_skill_level = 12 + ($rank_max * 2);
+	}	
+	$skill_array[$base_skills[$role]] = 1;	
+	if ($role != 'officer' && $role != 'troop' ) {
+		$skill_array = add_skill($skill_array, 'GunCbt') ;
+		$skill_array = add_skill($skill_array, 'Recon') ;
+	}
+	if ( $rank_min >= 4 ) {
+		$skill_array = add_skill($skill_array, 'Leadership') ;
+	}
+	for ( $i = 0; $i < $skill_count; $i++ ) {
+		$skill_array = add_skill($skill_array, $all_skills[rand(1, $max_skill_level)]);
+	}
+    $person_array = array(
+        'rank' => $rank,
+        'first_name' => $first_name,
+        'last_name' => $last_name,
+        'upp' => $upp,
+        'gender' => $gender,
+        'age' => $age,
+        'skills' => $skill_array
+        );
+    $person_string = json_encode($person_array);
+    fwrite($mercenary_js, $person_string);
+    fwrite($mercenary_js, "\n");	
+}
+
+function generate_troops_json() {
+    global $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills;
+
+    for ( $i = 0; $i <100; $i++){
+		generate_person_json( 10, 13, 'officer', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person_json( 14, 16, 'officer', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person_json( 5, 7, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person_json( 4, 5, 'nco', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+		generate_person_json( 2, 3, 'medic', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	    generate_person_json( 2, 3, 'comm', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	    generate_person_json( 2, 3, 'sensors', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	    generate_person_json( 2, 3, 'clerk', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+	    generate_person_json( 1, 2, 'driver', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+    }
+
+    for ($i = 0; $i < 10000; $i++) {
+		generate_person_json( 1, 1, 'troop', $stat_parameters, $last_names, $male_first_names, $female_first_names, $rank_structure, $base_skills, $all_skills) ;
+    }
+
+
+}
+
 function generate_squad($troop_count, $stat_parameters, $last_names, $male_first_names, $female_first_names, $unit_id, $unit_number, $rank_structure, $base_skills, $all_skills) {
-	
 	$UNIT = $unit_id[$unit_number];
 	echo "\n$UNIT Squad:\n";
 	
