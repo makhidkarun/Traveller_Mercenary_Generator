f187e1e1e661a804578bf5303fc1f7d41739aa6c
Leam Hall <leamhall@gmail.com>
2013-10-11 07:18:43 -0400

    Moved dice conventions into a helper class.

---
+++ Dice.php
@@ -0,0 +1,31 @@
+<?php
+
+// Dice.php
+// Helper class with dice and random number conventions.
+
+class Dice {
+
+    public function roll_dice($num_dice, $die_min, $die_max) {
+        $total = 0;
+        for ( $i = 1; $i <= $num_dice; $i++) {
+            $total += rand($die_min , $die_max);
+        }
+        return $total;
+    }
+
+    public function generate_upp() {
+        $upp = '';
+        for ( $i = 1; $i <= 6; $i++) {
+            // $stat_roll = rand(1,6) + rand(1,6);
+            $stat_roll = $this->roll_dice(2,1,6);
+            $stat = strtoupper(dechex($stat_roll));
+            $upp = $upp . $stat;
+        }
+        return $upp;
+    }
+
+}
+
+
+
+    
---
+++ test_Dice.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'Dice.php';
+
+class test_Dice extends Dice {
+
+
+    public function __construct() {
+        $my_upp = $this->generate_upp();
+        echo "My UPP: " . $my_upp . "\n";
+    }
+}
+
+$my_test = new test_Dice();
+
+?>
+
