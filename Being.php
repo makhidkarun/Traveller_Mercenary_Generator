bafd4eb34e0c34fa35588b02be4af19428d52ad4
Leam Hall <leamhall@gmail.com>
2013-10-11 07:18:06 -0400

    UPdated Being.php, removed rolls.

--- Being.php
+++ Being.php
@@ -1,29 +1,14 @@
 <?php
 
-class Being {
+// Being.php
+// To be implemented in various ways.
+abstract class Being {
 
-    protected $stats = array( 'STR' => 0,
-                            'DEX' => 0,
-                            'CON' => 0,
-                            'INT' => 0,
-                            'EDU' => 0,
-                            'SOC' => 0 );
+    protected $stats = array();
 
     protected $name;
 
-    public function __construct() {
-        echo "Name is " . $this->name;
-    }        
 
-    protected function roll($num_dice) {
-        $total = 0;
-        for ( $i = 0; $i <= $num_dice; $i++) {
-            $total += rand(1,6);
-        }
-        return $total;
-    }
-
-    protected function roll_stats($stats) {
 }
 
 
