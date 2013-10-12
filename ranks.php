641e94f58c60e92ecf4e7d8dc90a5147a925e962
Leam Hall <leamhall@gmail.com>
2013-10-12 11:24:57 -0400

    Added ranks file.

---
+++ ranks.php
@@ -0,0 +1,8 @@
+<?php
+
+$ranks = array();
+$ranks['enlisted'] = array(  1 => 'PVT', 'LCP', 'CPL', 'LSG', 'SGT', 'GSG', 'LDS', '1SG', 'SMG');
+$ranks['officer'] = array( 1 => '2LT', '1LT', 'CPT', 'MAJ', 'LTC', 'COL', 'BG', 'MG', 'LG', 'G');
+
+?>
+
