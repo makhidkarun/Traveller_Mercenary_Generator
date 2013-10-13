553e01a43791c5f053b8344475ac3e04c5ad4186
Leam Hall <leamhall@gmail.com>
2013-10-13 09:23:29 -0400

    Modified army_skills
    Renamed to mercenary_skills and collapsed the MOS[] array a level.
    This should make table selection easier.

---
+++ mercenary_skills.php
@@ -0,0 +1,49 @@
+<?php
+
+// mercenary_skills.php
+// Pretty much from Book 4, 1981 version.
+// Except for Mechanical vs Mechanic
+
+$mercenary_skills = array();
+
+$mercenary_skills['MOS_Infantry'][1] = 'GunCbt';
+$mercenary_skills['MOS_Infantry'][2] = 'GunCbt';
+$mercenary_skills['MOS_Infantry'][3] = 'HvyWpn';
+$mercenary_skills['MOS_Infantry'][4] = 'HvyWpn';
+$mercenary_skills['MOS_Infantry'][5] = 'Vehicle';
+$mercenary_skills['MOS_Infantry'][6] = 'Recon';
+$mercenary_skills['MOS_Infantry'][7] = 'VaccSuit';
+
+$mercenary_skills['MOS_Cavalry'][1] = 'Vehicle';
+$mercenary_skills['MOS_Cavalry'][2] = 'Vehicle';
+$mercenary_skills['MOS_Cavalry'][3] = 'Vehicle';
+$mercenary_skills['MOS_Cavalry'][4] = 'HvyWpn';
+$mercenary_skills['MOS_Cavalry'][5] = 'HvyWpn';
+$mercenary_skills['MOS_Cavalry'][6] = 'Mechanic';
+$mercenary_skills['MOS_Cavalry'][7] = 'Computer';
+
+$mercenary_skills['ArmyLife'][1] = 'Brawling';
+$mercenary_skills['ArmyLife'][2] = '+1 Str';
+$mercenary_skills['ArmyLife'][3] = 'Gambling';
+$mercenary_skills['ArmyLife'][4] = '+1 Dex';
+$mercenary_skills['ArmyLife'][5] = '+1 End';
+$mercenary_skills['ArmyLife'][6] = '+1 End';
+$mercenary_skills['ArmyLife'][7] = 'Gun-Pistol';
+$mercenary_skills['ArmyLife'][8] = '+1 Soc';
+$mercenary_skills['ArmyLife'][9] = '+1 Soc';
+
+$mercenary_skills['NCO'][1] = 'HvyWpn';
+$mercenary_skills['NCO'][2] = 'Mechanic';
+$mercenary_skills['NCO'][3] = 'Tactics';
+$mercenary_skills['NCO'][4] = 'HvyWpn';
+$mercenary_skills['NCO'][5] = 'Mechanic';
+$mercenary_skills['NCO'][6] = 'Tactics';
+$mercenary_skills['NCO'][7] = 'Leader';
+$mercenary_skills['NCO'][8] = 'Leader';
+$mercenary_skills['NCO'][9] = 'Admin';
+$mercenary_skills['NCO'][10] = 'Instruction';
+$mercenary_skills['NCO'][11] = 'Admin';
+
+
+?>
+
