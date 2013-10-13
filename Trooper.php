e464ed08c307315b96c17ab8cb431061574e7663
Leam Hall <leamhall@gmail.com>
2013-10-13 13:03:27 -0400

    Added skill_tables to Trooper. Still working on it.

--- Trooper.php
+++ Trooper.php
@@ -3,61 +3,69 @@
 // Trooper.php
 // With mods from MANCHUCK on #phpmentoring
 
+// Notes:
+//  https://gist.github.com/manchuck/6952598
+//  http://pastebin.com/3DQuBf1q
+
 require_once 'Being.php';
-require_once 'Dice.php';
-require_once 'ranks.php';
-require_once 'person_params.php';
-require_once 'RoleAbstract.php';
+require_once 'imperial_ranks.php';
+require_once 'MilitaryRoleAbstract.php';
+require_once 'TrooperParams.php';
+require_once 'NCOParams.php';
+require_once 'mercenary_skills.php';
+
 
 class Trooper extends Being {
 
-    public $rank;
+    private $rank, $mos;
     private $min_age, $max_age, $min_rank, $max_rank, $rank_group, $rank_roll;
 
-    public function __construct(RoleAbstract $role) {
-       // global $person_params; 
-    /*
-        switch($role) {
-            case 'trooper':
-                $min_age = 16;
-                $max_age = 18;
-                $min_rank = 1;
-                $max_rank = 2;
-                $rank_group = 'enlisted';
-                $skill_groups = array('MOS' => 0, 'Life');
-                break;
-            case 'nco':
-                $min_age = 20;
-                $max_age = 30;
-                $min_rank = 3;
-                $max_rank = 9;
-                $rank_group = 'enlisted';
-                $skill_groups = array('MOS' => 0, 'Life', 'NCO');
-                break;
-        }
-    */
-        // $rank_roll = mt_rand($role->get_min_rank(), $role->get_max_rank()$min_rank, $max_rank);
-       // $rank_roll = mt_rand($role->set_min_rank(), $role->set_max_rank());
-       // echo "Rank roll is $rank_roll.\n";
-       /* $this->gender = $this->set_gender(50);
+    public function __construct(MilitaryRoleAbstract $role) {
+ 
+        $this->gender = $this->set_gender(50);
         $this->name =  $this->set_name('humaniti', $this->gender);
-        $this->rank = $this->set_rank($rank_group, $rank_roll);
-        $this->age = $this->set_age($min_age, $max_age) + $rank_roll;
         $this->stats = $this->set_stats($this->stats);
         $this->upp = $this->set_upp($this->stats);
-        */ 
-        // $this->age = $this->set_age($role->get_min_age(), $role->get_max_age());
-        $this->age = $this->set_age($role->get_min_age(), $role->get_max_age());
-        echo "Age is " . $this->age . ".\n";
-       $rank_roll = mt_rand($role->get_min_rank(), $role->get_max_rank());
-        echo "rank_roll is $rank_roll.\n";
+        $rank_roll = mt_rand($role->get_min_rank(), $role->get_max_rank());
+        $this->age = $this->set_age($role->get_min_age(), $role->get_max_age()) + $rank_roll;
+        $this->rank = $this->set_rank($role->get_rank_group(), $rank_roll);
 
+        // Set the MOS and list of skill tables to choose from
+        $this->mos = $this->set_mos();       
+        $this->skill_tables = $this->add_skill_tables(&$this->skill_tables, 'ArmyLife');
+        $mos_table = 'MOS_' . $this->mos;
+        $this->skill_tables = $this->add_skill_tables(&$this->skill_tables, $mos_table);
+        if ( isset( $this->additional_skill_tables)) {
+            foreach ( $this->additional_skill_tables as $key => $value) {
+               $this->skill_tables = $this->add_skill_tables(&$this->skill_tables, $value);
+            }
+        }
+
+        // Start adding skills 
+        $this->skills = $this->add_skill($this->skills, 'GunCbt');
+ 
     }
 
     protected function set_rank($rank_group, $rank_roll) {
+        // This global will go away when I have a db for it.
         global $ranks;
         return $ranks[$rank_group][$rank_roll];
     }
-        
+       
+    public function get_rank() {
+        return $this->rank;
+    }
+
+    protected function set_mos() {
+        $mos_list = array('Infantry', 'Cavalry');
+        $mos_roll = mt_rand(0,1);
+        $mos = $mos_list[$mos_roll];
+        return $mos;
+    }
+    
+    public function get_mos() {
+        return $this->mos;
+    }
+ 
 }
 
