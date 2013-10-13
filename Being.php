24e63468fb8e21f9365eb0f0fe90d9eee2b54954
Leam Hall <leamhall@gmail.com>
2013-10-13 12:55:06 -0400

    Modified Being
    Working on the skill_tables stuff.

--- Being.php
+++ Being.php
@@ -9,13 +9,19 @@ require_once 'Dice.php';
 abstract class Being {
 
     public $stats = array('Str' => 0, 'Dex' => 0, 'End' => 0, 'Int' => 0, 'Edu' => 0, 'Soc' => 0);
-    public $skills = array();
-    public $upp;
-    public $name;
-    public $age;
+    protected $skills = array();
+    protected $upp;
+    protected $name;
+    protected $age;
+    protected $gender;
+    public $skill_tables = array();
 
     protected function set_age($min, $max) {
         return mt_rand($min, $max);
+    }
+
+    public function get_age() {
+        return $this->age;
     }
 
     protected function set_gender($percent_male) {
@@ -24,8 +30,11 @@ abstract class Being {
         if ( $roll <= $percent_male ) {
             $gender = 'male';
         }
-
         return $gender;
+    }
+    
+    public function get_gender() {
+        return $this->gender;
     }
 
     protected function set_name($race, $gender) {
@@ -41,6 +50,10 @@ abstract class Being {
  
         return $name;     
     }
+
+    public function get_name() {
+        return $this->name;
+    }
     
     protected function set_stats(&$stats) {
         foreach($stats as $stat => $value) {
@@ -50,21 +63,53 @@ abstract class Being {
         return $stats; 
     }
 
+    public function get_stat($stats, $stat) {
+        return $stats[$stat];
+    }
+
+    public function get_stat_hex($stats, $stat) {
+        return strtoupper(dechex($stats[$stat]));
+    }
+
     protected function set_upp($stats){
         $upp = '';
-        $upp = $upp . strtoupper(dechex($stats['Str'])); 
-        $upp = $upp . strtoupper(dechex($stats['Dex'])); 
-        $upp = $upp . strtoupper(dechex($stats['End'])); 
-        $upp = $upp . strtoupper(dechex($stats['Int'])); 
-        $upp = $upp . strtoupper(dechex($stats['Edu'])); 
-        $upp = $upp . strtoupper(dechex($stats['Soc'])); 
+        reset ($stats);
+        while (list($k, $v) = each($stats)) {
+            $upp = $upp . strtoupper(dechex($stats[$k])); 
+        }
         return $upp;
     }
 
+    public function get_upp() {
+        return $this->upp;
+    }
+        
+    public function add_skill(&$skills, $skill) {    
+        if (array_key_exists($skill, $skills)) {
+            $skills[$skill] += 1;
+        } else {
+            $skills[$skill] = 1;
+        }
+        return $skills;
+    }
 
-            
+    public function get_skills() {
+       return $this->skills;
+    }
+   
+    public function choose_skill ($skill_array, $skill_tables, $roll) {
+        $rand_table = array_rand($skill_tables);
+        $skill = $skill_array[$rand_table][$roll];
+        return $skill;
+    }
+
+    public function add_skill_tables(&$skill_tables, $table) {
+        $skill_tables[$table] = 0;
+        return $skill_tables;
+    } 
+
+    public function get_skill_tables() {
+        return $this->skill_tables;
+    }
+
 }
-
-
-
-    
