950ecdc973659fefad66c602d2758cf19ba2938e
Leam Hall <leamhall@gmail.com>
2013-10-13 07:56:58 -0400

    Added MilitaryRoleAbstract
    Was named RoleAbstract.

---
+++ MilitaryRoleAbstract.php
@@ -0,0 +1,16 @@
+<?php
+
+// RoleAbstract.php
+// From MAXCHUCK on #phpmentoring
+ 
+abstract class MilitaryRoleAbstract {
+
+    protected $min_age;
+    protected $max_age;
+    protected $min_rank;
+    protected $max_rank;
+    protected $rank_group;
+    protected $skill_groups = array( 'MOS' => 0, 'Life');
+
+}
+
