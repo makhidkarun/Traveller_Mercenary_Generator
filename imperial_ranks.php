4ef7c091f888522e3e87f269f0cea91b9a092eac
Leam Hall <leamhall@gmail.com>
2018-02-12 18:17:06 -0500

    Cleanup.

--- imperial_ranks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ranks = array();
-$ranks['enlisted'] = array(  1 => 'PVT', 'LCP', 'CPL', 'LSG', 'SGT', 'GSG', 'LDS', '1SG', 'SMG');
-$ranks['officer'] = array( 1 => '2LT', '1LT', 'CPT', 'MAJ', 'LTC', 'COL', 'BG', 'MG', 'LG', 'G');
-
-?>
-
